f26ef64c8cfb95d22e38e031dbf49c8bcbaf3827
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-16 21:18:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 data1 = [100, 90, 80, 60]
 data2 = [90, 80, 70, 100]
 labels = ['Math', 'English', 'Physics', 'Computer']
-names = ['Bill', 'Mary']
 
-fig, ax = plt.subplots()
 
 # ******************************
 # Make your code
 # ******************************
+fig, ax = plt.subplots()
+ax.bar(labels, data1)
+ax.bar(labels, data2, bottom=data1)
+ax.legend(names)
+ax.set_title('Stacked graph for scores')
 
+plt.show()
 
 fig.savefig('A10.png')
